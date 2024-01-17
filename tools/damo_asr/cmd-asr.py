a7b64b4d7e9a8724c5b49dfa0933c0138f3bd23c
刘悦 <zcxey2911@hotmail.com>
2024-01-17 23:29:26 +0800

    Update cmd-asr.py
    通过传入实例化模型对象，从而支持在推理过程中定制化语言模型，从而为将来的阿里ASR中日英三语转写做扩展

--- tools/damo_asr/cmd-asr.py
+++ tools/damo_asr/cmd-asr.py
@@ -2,13 +2,24 @@
 
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
+from modelscope.models import Model
 import sys,os,traceback
 dir=sys.argv[1]
 # opt_name=dir.split("\\")[-1].split("/")[-1]
 opt_name=os.path.basename(dir)
+
+# FunAsr三语转写model
+lang2model = {
+            'zh': 'tools/damo_asr/models/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+            'ja': "tools/damo_asr/models/speech_UniASR_asr_2pass-ja-16k-common-vocab93-tensorflow1-offline",
+            "en": "tools/damo_asr/models/speech_UniASR_asr_2pass-en-16k-common-vocab1080-tensorflow1-offline",
+}
+
+model = Model.from_pretrained(lang2model["zh"])
+
 inference_pipeline = pipeline(
     task=Tasks.auto_speech_recognition,
-    model='tools/damo_asr/models/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+    model=model,
     vad_model='tools/damo_asr/models/speech_fsmn_vad_zh-cn-16k-common-pytorch',
     punc_model='tools/damo_asr/models/punc_ct-transformer_zh-cn-common-vocab272727-pytorch',
 )
