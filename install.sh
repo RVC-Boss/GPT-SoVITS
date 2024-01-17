b8c4cbd9004b139e6cf6f3364ba174d9451aa3ba
Alex Z <AlexZhou1995@users.noreply.github.com>
2024-01-17 08:48:27 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+conda install gcc gxx ffmpeg cmake
+conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install -r requirements.txt
