2176c9fc92f87a04487cc71f3ef62cf929600e16
浩瀚猫 <howcanoewang@gmail.com>
2024-01-20 01:26:59 +0900

    fix pretrained_models path error
    Try to fix #21 about the `huggingface_hub.utils._validators.HFValidationError: Repo id must be in the form 'repo_name' or 'namespace/repo_name': 'GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large'. Use `repo_type` argument if needed.`

--- webui.py
+++ webui.py
@@ -645,9 +645,9 @@ with gr.Blocks(title="GPT-SoVITS WebUI") as app:
             with gr.Row():
                 exp_name = gr.Textbox(label="*实验/模型名", value="xxx", interactive=True)
                 gpu_info = gr.Textbox(label="显卡信息", value=gpu_info, visible=True, interactive=False)
-                pretrained_s2G = gr.Textbox(label="预训练的SoVITS-G模型路径", value="GPT_SoVITS/pretrained_models/s2G488k.pth", interactive=True)
-                pretrained_s2D = gr.Textbox(label="预训练的SoVITS-D模型路径", value="GPT_SoVITS/pretrained_models/s2D488k.pth", interactive=True)
-                pretrained_s1 = gr.Textbox(label="预训练的GPT模型路径", value="GPT_SoVITS/pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt", interactive=True)
+                pretrained_s2G = gr.Textbox(label="预训练的SoVITS-G模型路径", value="pretrained_models/s2G488k.pth", interactive=True)
+                pretrained_s2D = gr.Textbox(label="预训练的SoVITS-D模型路径", value="pretrained_models/s2D488k.pth", interactive=True)
+                pretrained_s1 = gr.Textbox(label="预训练的GPT模型路径", value="pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt", interactive=True)
             with gr.TabItem("1A-训练集格式化工具"):
                 gr.Markdown(value="输出logs/实验名目录下应有23456开头的文件和文件夹")
                 with gr.Row():
@@ -661,14 +661,14 @@ with gr.Blocks(title="GPT-SoVITS WebUI") as app:
                 gr.Markdown(value="1Aa-文本内容")
                 with gr.Row():
                     gpu_numbers1a = gr.Textbox(label="GPU卡号以-分割，每个卡号一个进程",value="%s-%s"%(gpus,gpus),interactive=True)
-                    bert_pretrained_dir = gr.Textbox(label="预训练的中文BERT模型路径",value="GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large",interactive=False)
+                    bert_pretrained_dir = gr.Textbox(label="预训练的中文BERT模型路径",value="pretrained_models/chinese-roberta-wwm-ext-large",interactive=False)
                     button1a_open = gr.Button("开启文本获取", variant="primary",visible=True)
                     button1a_close = gr.Button("终止文本获取进程", variant="primary",visible=False)
                     info1a=gr.Textbox(label="文本进程输出信息")
                 gr.Markdown(value="1Ab-SSL自监督特征提取")
                 with gr.Row():
                     gpu_numbers1Ba = gr.Textbox(label="GPU卡号以-分割，每个卡号一个进程",value="%s-%s"%(gpus,gpus),interactive=True)
-                    cnhubert_base_dir = gr.Textbox(label="预训练的SSL模型路径",value="GPT_SoVITS/pretrained_models/chinese-hubert-base",interactive=False)
+                    cnhubert_base_dir = gr.Textbox(label="预训练的SSL模型路径",value="pretrained_models/chinese-hubert-base",interactive=False)
                     button1b_open = gr.Button("开启SSL提取", variant="primary",visible=True)
                     button1b_close = gr.Button("终止SSL提取进程", variant="primary",visible=False)
                     info1b=gr.Textbox(label="SSL进程输出信息")
