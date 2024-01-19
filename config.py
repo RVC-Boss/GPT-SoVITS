29261695ecb0e6872bb1f5066d205e890c819f42
謬紗特 <66856838+Miuzarte@users.noreply.github.com>
2024-01-19 14:25:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import sys
-is_half=True
-exp_root="logs"
-python_exec=sys.executable or "python"
-infer_device="cuda"
 
-webui_port_main=9874
-webui_port_uvr5=9873
-webui_port_infer_tts=9872
-webui_port_subfix=9871
+is_half = True
+exp_root = "logs"
+python_exec = sys.executable or "python"
+infer_device = "cuda"
+
+webui_port_main = 9874
+webui_port_uvr5 = 9873
+webui_port_infer_tts = 9872
+webui_port_subfix = 9871
+
+api_port = 9880
