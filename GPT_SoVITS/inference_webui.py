19ff34260b20149e0316010f75b1c115a3b8f9f3
RVC-Boss <129054828+RVC-Boss@users.noreply.github.com>
2024-01-21 23:02:33 +0800

    Add files via upload

--- GPT_SoVITS/inference_webui.py
+++ GPT_SoVITS/inference_webui.py
@@ -12,6 +12,8 @@ bert_path = os.environ.get(
 )
 infer_ttswebui = os.environ.get("infer_ttswebui", 9872)
 infer_ttswebui = int(infer_ttswebui)
+is_share = os.environ.get("is_share", "False")
+is_share=eval(is_share)
 if "_CUDA_VISIBLE_DEVICES" in os.environ:
     os.environ["CUDA_VISIBLE_DEVICES"] = os.environ["_CUDA_VISIBLE_DEVICES"]
 is_half = eval(os.environ.get("is_half", "True"))
