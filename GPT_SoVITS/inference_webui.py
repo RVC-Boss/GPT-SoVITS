16196b6f38fe1dd381440fabb434976669d47b40
刘悦 <zcxey2911@hotmail.com>
2024-01-26 15:42:49 +0800

    增加引导音频记忆、引导音频文本记忆、语种记忆
    增加引导音频记忆、引导音频文本记忆、语种记忆

--- GPT_SoVITS/inference_webui.py
+++ GPT_SoVITS/inference_webui.py
@@ -418,10 +418,10 @@ with gr.Blocks(title="GPT-SoVITS WebUI") as app:
             GPT_dropdown.change(change_gpt_weights,[GPT_dropdown],[])
         gr.Markdown(value=i18n("*请上传并填写参考信息"))
         with gr.Row():
-            inp_ref = gr.Audio(label=i18n("请上传参考音频"), type="filepath")
-            prompt_text = gr.Textbox(label=i18n("参考音频的文本"), value="")
+            inp_ref = gr.Audio(label=i18n("请上传参考音频"), type="filepath",value=upload_audio_path)
+            prompt_text = gr.Textbox(label=i18n("参考音频的文本"), value=upload_audio_text)
             prompt_language = gr.Dropdown(
-                label=i18n("参考音频的语种"),choices=[i18n("中文"),i18n("英文"),i18n("日文")],value=i18n("中文")
+                label=i18n("参考音频的语种"),choices=[i18n("中文"),i18n("英文"),i18n("日文")],value=i18n(upload_audio_lanuage)
             )
         gr.Markdown(value=i18n("*请填写需要合成的目标文本"))
         with gr.Row():
