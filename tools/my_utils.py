1a876935288a160bb996f9b9a7d9a607e7ae97ba
RVC-Boss <129054828+RVC-Boss@users.noreply.github.com>
2024-01-28 21:47:27 +0800

    Update my_utils.py

--- tools/my_utils.py
+++ tools/my_utils.py
@@ -1,4 +1,4 @@
-import platform,os
+import platform,os,traceback
 import ffmpeg
 import numpy as np
 
@@ -9,12 +9,17 @@ def load_audio(file, sr):
         # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
         # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
         file = clean_path(file)  # 防止小白拷路径头尾带了空格和"和回车
+        if os.path.exists(file) == False:
+            raise RuntimeError(
+                "You input a wrong audio path that does not exists, please fix it!"
+            )
         out, _ = (
             ffmpeg.input(file, threads=0)
             .output("-", format="f32le", acodec="pcm_f32le", ac=1, ar=sr)
             .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
         )
     except Exception as e:
+        traceback.print_exc()
         raise RuntimeError(f"Failed to load audio: {e}")
 
     return np.frombuffer(out, np.float32).flatten()
