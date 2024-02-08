8e1d8b6c102cd5bdaec911e91dcc2713b6aa1126
SapphireLab <36986837+SapphireLab@users.noreply.github.com>
2024-02-09 06:26:48 +0800

    更新funasr本地路径

--- tools/asr/funasr_asr.py
+++ tools/asr/funasr_asr.py
@@ -7,9 +7,9 @@ from tqdm import tqdm
 
 from funasr import AutoModel
 
-path_asr  = 'tools/damo_asr/models/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch'
-path_vad  = 'tools/damo_asr/models/speech_fsmn_vad_zh-cn-16k-common-pytorch'
-path_punc = 'tools/damo_asr/models/punc_ct-transformer_zh-cn-common-vocab272727-pytorch'
+path_asr  = 'tools/asr/models/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch'
+path_vad  = 'tools/asr/models/speech_fsmn_vad_zh-cn-16k-common-pytorch'
+path_punc = 'tools/asr/models/punc_ct-transformer_zh-cn-common-vocab272727-pytorch'
 path_asr  = path_asr  if os.path.exists(path_asr)  else "iic/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
 path_vad  = path_vad  if os.path.exists(path_vad)  else "iic/speech_fsmn_vad_zh-cn-16k-common-pytorch"
 path_punc = path_punc if os.path.exists(path_punc) else "iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch"
