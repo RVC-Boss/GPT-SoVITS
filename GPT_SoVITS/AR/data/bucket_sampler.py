4496426896157f441ef090ecca11c759369f8a37
root <root@debian>
2024-02-28 17:31:19 +0800

    修改代码引用，淡定

--- GPT_SoVITS/AR/data/bucket_sampler.py
+++ GPT_SoVITS/AR/data/bucket_sampler.py
@@ -1,4 +1,5 @@
-# modified from https://github.com/feng-yufei/shared_debugging_code/blob/main/bucketsampler.py
+# modified from https://github.com/yangdongchao/SoundStorm/blob/master/soundstorm/s1/AR/data/bucket_sampler.py
+# reference: https://github.com/lifeiteng/vall-e
 import itertools
 import math
 import random
