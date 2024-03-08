ac9a39b3cf12f75f9dbb1fd860c9c618f4462a2d
XTer <xxoy1234@outlook.com>
2024-03-09 00:07:35 +0800

    修改了一些bat

--- 0 一键启动脚本/0 一键更新项目.bat
+++ 0 一键启动脚本/0 一键更新项目.bat
@@ -2,24 +2,22 @@ CHCP 65001
 @echo off
 setlocal
 
-:: 设置 PortableGit 的路径
-set GIT_PATH=../PortableGit/bin
 
 :: 设置需要同步的本地仓库路径
 set REPO_PATH=../
 
-:: 添加 PortableGit 到 PATH，以便可以执行 git 命令
-set PATH=%GIT_PATH%;%PATH%
-
 :: 切换到仓库目录
 cd /d %REPO_PATH%
 
+:: 设置 PortableGit 的路径
+set GIT_PATH=PortableGit/bin
+
 echo 更新所有子模块
-git submodule update --init --recursive
+"%GIT_PATH%\git.exe" submodule update --init --recursive
 
 echo 执行 git pull 更新本地仓库
-git stash
-git pull https://github.com/X-T-E-R/GPT-SoVITS-Inference.git main
+"%GIT_PATH%\git.exe" stash
+"%GIT_PATH%\git.exe" pull https://github.com/X-T-E-R/GPT-SoVITS-Inference.git main
 
 echo.
 echo 更新完成！
