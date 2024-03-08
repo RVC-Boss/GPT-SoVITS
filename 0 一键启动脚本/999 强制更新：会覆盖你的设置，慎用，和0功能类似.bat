ee59cf8005f0fc04a8442719258c163f0b12d858
XTer <xxoy1234@outlook.com>
2024-03-09 00:07:39 +0800

    modified:   "0 \344\270\200\351\224\256\345\220\257\345\212\250\350\204\232\346\234\254/999 \345\274\272\345\210\266\346\233\264\346\226\260\357\274\232\344\274\232\350\246\206\347\233\226\344\275\240\347\232\204\350\256\276\347\275\256\357\274\214\346\205\216\347\224\250\357\274\214\345\222\2140\345\212\237\350\203\275\347\261\273\344\274\274.bat"

--- 0 一键启动脚本/999 强制更新：会覆盖你的设置，慎用，和0功能类似.bat
+++ 0 一键启动脚本/999 强制更新：会覆盖你的设置，慎用，和0功能类似.bat
@@ -2,25 +2,25 @@ CHCP 65001
 @echo off
 setlocal
 
-:: 设置 PortableGit 的路径
-set GIT_PATH=../PortableGit/bin
 
 :: 设置需要同步的本地仓库路径
-set REPO_PATH=./
-
-:: 添加 PortableGit 到 PATH，以便可以执行 git 命令
-set PATH=%GIT_PATH%;%PATH%
+set REPO_PATH=../
 
 :: 切换到仓库目录
 cd /d %REPO_PATH%
 
+:: 设置 PortableGit 的路径
+set GIT_PATH=PortableGit/bin
+
+
+
 echo 强制覆盖所有子模块
-git submodule update --init --recursive
-git submodule foreach --recursive "git fetch origin plug_in && git reset --hard origin/plug_in"
+"%GIT_PATH%\git.exe" submodule update --init --recursive
+"%GIT_PATH%\git.exe" submodule foreach --recursive "git fetch origin plug_in && git reset --hard origin/plug_in"
 
 echo 执行 git pull 更新本地仓库
-git fetch https://github.com/X-T-E-R/GPT-SoVITS-Inference.git main
-git reset --hard FETCH_HEAD
+"%GIT_PATH%\git.exe" fetch https://github.com/X-T-E-R/GPT-SoVITS-Inference.git main
+"%GIT_PATH%\git.exe" reset --hard FETCH_HEAD
 
 echo.
 echo 更新完成！
