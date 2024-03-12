4ae06643dddf1910d435c8269ddd954cd7220c12
XTer <xxoy1234@outlook.com>
2024-03-12 23:58:08 +0800

    修正了浏览器打开时间

--- 0 一键启动脚本/10 启动模型管理界面（可选）.bat
+++ 0 一键启动脚本/10 启动模型管理界面（可选）.bat
@@ -2,7 +2,6 @@ CHCP 65001
 @echo off 
 cd ../
 echo 尝试启动程序
-start  http://127.0.0.1:9868
 runtime\python.exe ./Inference/src/Character_Manager.py
 
 pause
--- 0 一键启动脚本/4 启动前端合成程序（可选，依赖后端）.bat
+++ 0 一键启动脚本/4 启动前端合成程序（可选，依赖后端）.bat
@@ -2,7 +2,6 @@ CHCP 65001
 @echo off 
 cd ../
 echo 尝试启动程序
-start  http://127.0.0.1:9867
 runtime\python.exe ./Inference/src/TTS_Webui.py
 
 pause
