3b3cd6d56c0cee4fb9d9e298ed8d7369bdb813aa
XTer <xxoy1234@outlook.com>
2024-03-13 00:10:30 +0800

    修改了一些问题

--- 0 一键启动脚本/10 启动模型管理界面（可选）.bat
+++ 0 一键启动脚本/10 启动模型管理界面（可选）.bat
@@ -1,7 +1,8 @@
 CHCP 65001
 @echo off 
 cd ../
-echo 尝试启动程序
+echo 尝试启动程序，请耐心等待gradio启动，等待十几秒
+start  http://127.0.0.1:9868
 runtime\python.exe ./Inference/src/Character_Manager.py
 
 pause
--- 0 一键启动脚本/4 启动前端合成程序（可选，依赖后端）.bat
+++ 0 一键启动脚本/4 启动前端合成程序（可选，依赖后端）.bat
@@ -1,7 +1,8 @@
 CHCP 65001
 @echo off 
 cd ../
-echo 尝试启动程序
+echo 尝试启动程序，请耐心等待gradio启动，等待十几秒
+start  http://127.0.0.1:9867
 runtime\python.exe ./Inference/src/TTS_Webui.py
 
 pause
