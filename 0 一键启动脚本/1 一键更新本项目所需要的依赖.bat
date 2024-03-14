8ee1ac18ae58f487265e60a5aebc599004836c2e
XTer <xxoy1234@outlook.com>
2024-03-15 03:36:50 +0800

    增加了git源信任

--- 0 一键启动脚本/1 一键更新本项目所需要的依赖.bat
+++ 0 一键启动脚本/1 一键更新本项目所需要的依赖.bat
@@ -3,6 +3,7 @@ CHCP 65001
 cd ../
 echo 请确保您的主项目运行正常
 runtime\python.exe -m pip  config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+runtime\python.exe -m pip  config set install.trusted-host pypi.tuna.tsinghua.edu.cn
 runtime\python.exe -m pip install -r ./requirements.txt
 
 pause
