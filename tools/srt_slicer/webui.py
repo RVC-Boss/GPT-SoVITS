ccf832c4d8797dd5ac240f10f9cee1499b97bf3c
XTer <xxoy1234@outlook.com>
2024-03-21 15:23:15 +0800

    修正了语言翻译

--- tools/srt_slicer/webui.py
+++ tools/srt_slicer/webui.py
@@ -4,6 +4,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append('.')
 from srt_utils import (
     merge_subtitles_with_lib,
