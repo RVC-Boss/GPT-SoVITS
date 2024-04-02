abd3a357586a67fae0c260d3e87f6ef3b57e2bd5
kahwaipd <100838692+kahwaipd@users.noreply.github.com>
2024-04-02 14:53:29 +0800

    Update config.py
    updated default pertained model paths

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ is_half = True if is_half_str.lower() == 'true' else False
 is_share_str = os.environ.get("is_share","False")
 is_share= True if is_share_str.lower() == 'true' else False
 
-cnhubert_path = "GPT_SoVITS/pretrained_models/chinese-hubert-base"
-bert_path = "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
-pretrained_sovits_path = "GPT_SoVITS/pretrained_models/s2G488k.pth"
-pretrained_gpt_path = "GPT_SoVITS/pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt"
+cnhubert_path = "/app/data/c1/chinese-hubert-base"
+bert_path = "/app/data/c1/chinese-roberta-wwm-ext-large"
+pretrained_sovits_path = "/app/data/c1/sovits.pth"
+pretrained_gpt_path = "/app/data/c1/gpt.ckpt"
 
 exp_root = "logs"
 python_exec = sys.executable or "python"
