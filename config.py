5e6352aafbbbddade7a7fe9ed2b25e016bf6671d
XXXXRT666 <runtongzhong@gmail.com>
2024-05-19 15:38:03 +0100

    Added more extensions and converted them into a set.

--- config.py
+++ config.py
@@ -15,18 +15,32 @@ bert_path = "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
 pretrained_sovits_path = "GPT_SoVITS/pretrained_models/s2G488k.pth"
 pretrained_gpt_path = "GPT_SoVITS/pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt"
 
-AUDIO_EXTENSIONS = [
+AUDIO_EXTENSIONS = {
     "mp3",
     "wav",
+    "aac",
     "flac",
     "ogg",
-    "m4a",
     "wma",
-    "aac",
     "aiff",
-    "aif",
-    "aifc",
-]
+    "alac",
+    "m4a",
+    "opus",
+    "ra",
+    "amr",
+    "dts"
+    "mpc",
+    "tta",
+    "aiff",
+    "au",
+    "3gp",
+    "mka",
+    "voc",
+    "ape",
+    "caf",
+    "m3u",
+    "pls",
+}
 
 exp_root = "logs"
 python_exec = sys.executable or "python"
