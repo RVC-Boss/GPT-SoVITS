ff8a0ea4a0a99746166d9c5e07be2c179c67e6bd
Ziyao Wang <ziyaowang318@gmail.com>
2024-08-01 19:34:59 +0800

    Fix RegEX

--- GPT_SoVITS/utils.py
+++ GPT_SoVITS/utils.py
@@ -255,7 +255,7 @@ def clean_checkpoints(path_to_models="logs/44k/", n_ckpts_to_keep=2, sort_by_tim
         for f in os.listdir(path_to_models)
         if os.path.isfile(os.path.join(path_to_models, f))
     ]
-    name_key = lambda _f: int(re.compile("._(\d+)\.pth").match(_f).group(1))
+    name_key = lambda _f: int(re.compile(r"._(\d+)\.pth").match(_f).group(1))
     time_key = lambda _f: os.path.getmtime(os.path.join(path_to_models, _f))
     sort_key = time_key if sort_by_time else name_key
     x_sorted = lambda _x: sorted(
