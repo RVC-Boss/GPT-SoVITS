bcc883844b10c5e070e1f211ee382fb403db3c7f
XXXXRT666 <runtongzhong@gmail.com>
2024-08-02 18:37:01 +0800

    chores

--- tools/my_utils.py
+++ tools/my_utils.py
@@ -30,4 +30,4 @@ def clean_path(path_str:str):
         return clean_path(path_str[0:-1])
     if platform.system() == 'Windows':
         path_str = path_str.replace('/', '\\')
-    return path_str.strip(" ").strip('"').strip("\n").strip('"').strip(" ").strip("\u202a")
+    return path_str.strip(" ").strip('\'').strip("\n").strip('"').strip(" ").strip("\u202a")
