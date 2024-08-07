494e5da049acf73a9f2a07282c2decc5c7fa66b1
RVC-Boss <129054828+RVC-Boss@users.noreply.github.com>
2024-08-07 16:17:13 +0800

    Update 2-get-hubert-wav32k.py

--- GPT_SoVITS/prepare_datasets/2-get-hubert-wav32k.py
+++ GPT_SoVITS/prepare_datasets/2-get-hubert-wav32k.py
@@ -18,7 +18,7 @@ from scipy.io import wavfile
 import librosa
 now_dir = os.getcwd()
 sys.path.append(now_dir)
-from tools.my_utils import load_audio
+from tools.my_utils import load_audio,clean_path
 
 # from config import cnhubert_base_path
 # cnhubert.cnhubert_base_path=cnhubert_base_path
@@ -100,6 +100,7 @@ for line in lines[int(i_part)::int(all_parts)]:
     try:
         # wav_name,text=line.split("\t")
         wav_name, spk_name, language, text = line.split("|")
+        wav_name=clean_path(wav_name)
         if (inp_wav_dir != "" and inp_wav_dir != None):
             wav_name = os.path.basename(wav_name)
             wav_path = "%s/%s"%(inp_wav_dir, wav_name)
