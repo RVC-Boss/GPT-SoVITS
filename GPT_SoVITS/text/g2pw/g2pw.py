6d82050b9cce3cd42c573b2179975a25ab5467af
刘悦 <zcxey2911@hotmail.com>
2024-10-30 16:38:05 +0800

    add encoding (#1730)

--- GPT_SoVITS/text/g2pw/g2pw.py
+++ GPT_SoVITS/text/g2pw/g2pw.py
@@ -127,14 +127,14 @@ def get_dict():
 
 def read_dict():
     polyphonic_dict = {}
-    with open(PP_DICT_PATH) as f:
+    with open(PP_DICT_PATH,encoding="utf-8") as f:
         line = f.readline()
         while line:
             key, value_str = line.split(':')
             value = eval(value_str.strip())
             polyphonic_dict[key.strip()] = value
             line = f.readline()
-    with open(PP_FIX_DICT_PATH) as f:
+    with open(PP_FIX_DICT_PATH,encoding="utf-8") as f:
         line = f.readline()
         while line:
             key, value_str = line.split(':')
