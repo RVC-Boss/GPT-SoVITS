63a412bda55f5a58356c5193e32eb339d7ca4b48
Jarod Mica <jarodmica@gmail.com>
2024-12-23 02:07:09 -0800

    .

--- GPT_SoVITS/TTS_infer_pack/TTS.py
+++ GPT_SoVITS/TTS_infer_pack/TTS.py
@@ -29,8 +29,8 @@ from GPT_SoVITS.AR.models.t2s_lightning_module import Text2SemanticLightningModu
 from GPT_SoVITS.feature_extractor.cnhubert import CNHubert
 from GPT_SoVITS.module.models import SynthesizerTrn
 from GPT_SoVITS.module.mel_processing import spectrogram_torch
-from GPT_SoVITS.toolsi18n.i18n import I18nAuto, scan_language_list
-from GPT_SoVITS.toolsmy_utils import load_audio
+from GPT_SoVITS.tools.i18n.i18n import I18nAuto, scan_language_list
+from GPT_SoVITS.tools.my_utils import load_audio
 from transformers import AutoModelForMaskedLM, AutoTokenizer
 from GPT_SoVITS.TTS_infer_pack.text_segmentation_method import splits
 from GPT_SoVITS.TTS_infer_pack.TextPreprocessor import TextPreprocessor
