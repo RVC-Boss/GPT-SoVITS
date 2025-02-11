00ab793ff23d4000384d25ebf13df511bf6722b6
RVC-Boss <129054828+RVC-Boss@users.noreply.github.com>
2025-02-11 21:09:17 +0800

    gpt_sovits_v3
    gpt_sovits_v3

---
+++ GPT_SoVITS/f5_tts/model/__init__.py
@@ -0,0 +1,13 @@
+# from f5_tts.model.cfm import CFM
+#
+# from f5_tts.model.backbones.unett import UNetT
+from GPT_SoVITS.f5_tts.model.backbones.dit import DiT
+# from f5_tts.model.backbones.dit import DiTNoCond
+# from f5_tts.model.backbones.dit import DiTNoCondNoT
+# from f5_tts.model.backbones.mmdit import MMDiT
+
+# from f5_tts.model.trainer import Trainer
+
+
+# __all__ = ["CFM", "UNetT", "DiT", "MMDiT", "Trainer"]
+# __all__ = ["CFM", "UNetT", "DiTNoCond","DiT", "MMDiT"]
