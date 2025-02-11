190dd6198c0e475331fd678d4f05590b4e8687df
RVC-Boss <129054828+RVC-Boss@users.noreply.github.com>
2025-02-11 21:10:47 +0800

    gpt_sovits_v3
    gpt_sovits_v3

--- GPT_SoVITS/BigVGAN/tests/test_activation_snake_beta.py
+++ GPT_SoVITS/BigVGAN/tests/test_activation_snake_beta.py
@@ -9,7 +9,7 @@ sys.path.append(parent_dir)
 
 import torch
 from alias_free_activation.cuda import activation1d
-from activations import Snake
+from activations import SnakeBeta
 
 
 def test_load_fused_kernels():
@@ -23,14 +23,14 @@ def test_load_fused_kernels():
 def test_anti_alias_activation():
     data = torch.rand((10, 10, 200), device="cuda")
 
-    # Check activations.Snake cuda vs. torch
+    # Check activations, Snake CUDA vs. Torch
     fused_anti_alias_activation = activation1d.Activation1d(
-        activation=Snake(10), fused=True
+        activation=SnakeBeta(10), fused=True
     ).cuda()
     fused_activation_output = fused_anti_alias_activation(data)
 
     torch_anti_alias_activation = activation1d.Activation1d(
-        activation=Snake(10), fused=False
+        activation=SnakeBeta(10), fused=False
     ).cuda()
     torch_activation_output = torch_anti_alias_activation(data)
 
@@ -55,6 +55,7 @@ def test_anti_alias_activation():
             f"\n > fused_values={fused_activation_output[-1][-1][:].tolist()}, "
             f"\n > torch_values={torch_activation_output[-1][-1][:].tolist()}"
         )
+
 
 
 if __name__ == "__main__":
