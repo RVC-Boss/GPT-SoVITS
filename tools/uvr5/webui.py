e35ade8f609f7d9f9b728653c5d40a5522b4a3cf
starylan <starylan@outlook.com>
2025-02-28 01:41:04 +0800

    拆分i18n

--- tools/uvr5/webui.py
+++ tools/uvr5/webui.py
@@ -133,7 +133,7 @@ def uvr(model_name, inp_root, save_root_vocal, paths, save_root_ins, agg, format
 with gr.Blocks(title="UVR5 WebUI") as app:
     gr.Markdown(
         value=
-            i18n("本软件以MIT协议开源, 作者不对软件具备任何控制力, 使用软件者、传播软件导出的声音者自负全责. <br>如不认可该条款, 则不能使用或引用软件包内任何代码和文件. 详见根目录<b>LICENSE</b>.")
+            i18n("本软件以MIT协议开源, 作者不对软件具备任何控制力, 使用软件者、传播软件导出的声音者自负全责.") + "<br>" + i18n("如不认可该条款, 则不能使用或引用软件包内任何代码和文件. 详见根目录LICENSE.")
     )
     with gr.Group():
         gr.Markdown(html_center(i18n("伴奏人声分离&去混响&去回声"),'h2'))
