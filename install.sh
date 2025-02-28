26dd087fa224045dc4f967a649ff0214994e55b3
崔永亮 <702625325@qq.com>
2025-02-28 19:49:52 +0800

    修复 Linux 无法编译 pyopenjtalk

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-conda install -c conda-forge gcc
+conda install -c conda-forge gcc=14
 conda install -c conda-forge gxx
 conda install ffmpeg cmake
 conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=11.8 -c pytorch -c nvidia
