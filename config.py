ec15e48e6ef3ed93bc17494d11196c447b151ba9
samiabat <samuelabatneh20@gmail.com>
2025-04-17 23:14:11 +0300

    add version two

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ is_share= True if is_share_str.lower() == 'true' else False
 
 cnhubert_path = "GPT_SoVITS/pretrained_models/chinese-hubert-base"
 bert_path = "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
-pretrained_sovits_path = "GPT_SoVITS/pretrained_models/s2G488k.pth"
-pretrained_gpt_path = "GPT_SoVITS/pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt"
+pretrained_sovits_path = "GPT_SoVITS/pretrained_models/s2G2333k.pth"
+pretrained_gpt_path = "GPT_SoVITS/pretrained_models/s1bert25hz-5kh-longer-epoch=12-step=369668.ckpt"
 
 exp_root = "logs"
 python_exec = sys.executable or "python"
