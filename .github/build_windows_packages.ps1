659bd1b79040b1a393788c7b3bd1151a2307a8fb
XXXXRT666 <157766680+XXXXRT666@users.noreply.github.com>
2025-05-12 18:22:31 +0100

    WIP

--- .github/build_windows_packages.ps1
+++ .github/build_windows_packages.ps1
@@ -23,7 +23,7 @@ Invoke-WebRequest $pyUrl -OutFile $zst
 Get-ChildItem 
 Get-ChildItem $tmpDir
 Write-Host "0000"
-& "C:\Program Files\7-Zip\7z.exe" e $zst -o$tmpDir -aoa
+Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "e `"$zst`" -aoa" -WorkingDirectory $tmpDir -Wait
 Get-ChildItem $tmpDir
 Write-Host "11111"
 $tar = Get-ChildItem "$tmpDir" -Filter "*.tar" | Select-Object -First 1
