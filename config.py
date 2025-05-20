3093eafbe3c70bf03f59430b7452a9a8c5159096
samiabat <samuelabatneh20@gmail.com>
2025-05-20 15:41:29 +0300

    change to version 3

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ is_share= True if is_share_str.lower() == 'true' else False
 
 cnhubert_path = "GPT_SoVITS/pretrained_models/chinese-hubert-base"
 bert_path = "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
-pretrained_sovits_path = "GPT_SoVITS/pretrained_models/s2G2333k.pth"
-pretrained_gpt_path = "GPT_SoVITS/pretrained_models/s1bert25hz-5kh-longer-epoch=12-step=369668.ckpt"
+pretrained_sovits_path = "GPT_SoVITS/pretrained_models/s2Gv3.pth"
+pretrained_gpt_path = "GPT_SoVITS/pretrained_models/s1v3.ckpt"
 
 exp_root = "logs"
 python_exec = sys.executable or "python"
